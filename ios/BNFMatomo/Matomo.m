fcb5abf8e2403c0c7e2af7676a167390ea831c80
Jens Janiuk <jens.janiuk@actidoo.com>
2019-03-13 15:46:31 +0100

    fix user agent

--- ios/BNFMatomo/Matomo.m
+++ ios/BNFMatomo/Matomo.m
@@ -36,12 +36,12 @@ static NSString * const MatamoAppTrackingKey = @"@@Matamo-tracking-version@@";
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(initTracker: (NSString*)url id:(NSNumber* _Nonnull) id)
+RCT_EXPORT_METHOD(initTracker: (NSString*)url id:(NSNumber* _Nonnull) id userAgent:(NSString*)userAgent)
 {
 #if DEBUG
     RCTLogInfo(@"Initing tracker");
 #endif
-    tracker = [[MatomoTracker alloc] initWithSiteId:[id stringValue] baseURL:[NSURL URLWithString: url] userAgent: nil];
+    tracker = [[MatomoTracker alloc] initWithSiteId:[id stringValue] baseURL:[NSURL URLWithString: url] userAgent: userAgent];
 }
 
 RCT_EXPORT_METHOD(setUserId:(NSString* _Nonnull)userID)
